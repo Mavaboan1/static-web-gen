51d510fd7144b9fa7e6cfbff42fc3f49cfbaa27a
Mavaboan1 <mathias.valdemar.boiskau@gmail.com>
2025-07-12 23:48:49 +0200

    Started Project and finished Nodes

---
+++ src/htmlnode.py
@@ -0,0 +1,51 @@
+
+
+class HTMLNode:
+    def __init__(self, tag=None, value=None, children=None, props=None):
+        self.tag = tag
+        self.value = value
+        self.children = children
+        self.props = props
+
+    def to_html(self):
+        raise NotImplementedError("to_html not implemented")
+    def props_to_html(self):
+        if self.props is None:
+            return ""
+        props_html = ""
+        for prop in self.props:
+            props_html += f' {prop}="{self.props[prop]}"'
+        return props_html
+
+    def __repr__(self):
+        return f"HTMLNode(Tag: {self.tag}, Value: {self.value}, Children: {self.children}, Props: {self.props})"
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, None, props)
+
+    def to_html(self):
+        if self.value is None:
+            raise ValueError("Leaf node must have a value")
+        if self.tag is None:
+            return self.value
+        return f'<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>'
+    def __repr__(self):
+        return f"LeafNode(Tag: {self.tag}, Value: {self.value}, Props: {self.props})"
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag, None, children, props)
+
+    def to_html(self):
+        if self.tag is None:
+            raise ValueError("Parent node must have a TAG")
+        if self.children is None:
+            raise ValueError("Parent node must have CHILDREN")
+        final = ""
+        for child in self.children:
+            final += child.to_html()
+        return f'<{self.tag}{self.props_to_html()}>{final}</{self.tag}>'
+
+    def __repr__(self):
+        return f"ParentNode(Tag: {self.tag}, Children: {self.children}, Props: {self.props})"
